7b30d98dbcdbec9142eeab2a6673b62a0c6acb14
ST10467193_Tristan_Kriel <ST10467193@vcconnect.edu.za>
2025-09-04 02:38:39 +0200

    SeriesModel

--- src/seriesapplication/SeriesModel.java
+++ src/seriesapplication/SeriesModel.java
@@ -1,26 +1,23 @@
 package seriesapplication;
 
+
 public class SeriesModel {
     public String SeriesID;
     public String SeriesName;
     public String SeriesAge;
     public String SeriesNumberOfEpisodes;
     
-    // Constructor
-    public SeriesModel(String SeriesID, String SeriesName, 
-                      String SeriesAge, String SeriesNumberOfEpisodes) {
+    //CONSTRUCTOR
+    public SeriesModel(String SeriesID, String SeriesName, String SeriesAge, String SeriesNumberOfEpisodes) {
         this.SeriesID = SeriesID;
         this.SeriesName = SeriesName;
         this.SeriesAge = SeriesAge;
         this.SeriesNumberOfEpisodes = SeriesNumberOfEpisodes;
     }
     
-    // Override toString() for easier displaying
+    //Override toString() for easy displaying
     @Override
     public String toString() {
-        return "Series ID: " + SeriesID + 
-               ", Name: " + SeriesName + 
-               ", Age Rating: " + SeriesAge + 
-               ", Episodes: " + SeriesNumberOfEpisodes;
+        return "SERIES ID: " + SeriesID + "\nSERIES NAME: " + SeriesName + "\nSERIES AGE RESTRICTION: " + SeriesAge + "\nNUMBER OF EPISODES: " + SeriesNumberOfEpisodes;
     }
 }
