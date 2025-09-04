a0c6c21276b2e66d8425d35aa91c55a88a5276ca
ST10467193_Tristan_Kriel_BCA1_PROG5112_GR02 <krieltristan68@gmail.com>
2025-09-04 02:41:49 +0200

    Revert "SeriesModel"
    This reverts commit b03a92fbadab2538a5bfea23ecba59e513d5a55f.

---
+++ src/seriesapplication/seriesModel.java
@@ -0,0 +1,17 @@
+package seriesapplication;
+
+public class seriesModel {
+    public String SeriesID;
+    public String SeriesName;
+    public String SeriesAge;
+    public String SeriesNumberOfEpisodes;
+    
+//-----------------------------------------------CONSTRUCTOR-----------------------------------------------------------------------------
+    public seriesModel(String SeriesID, String SeriesName, String SeriesAge, String SeriesNumberOfEpisodes) {
+        this.SeriesID = SeriesID;
+        this.SeriesName = SeriesName;
+        this.SeriesAge = SeriesAge;
+        this.SeriesNumberOfEpisodes = SeriesNumberOfEpisodes;
+    }
+    
+}
